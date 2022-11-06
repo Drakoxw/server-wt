944a91b101ab298138c9c092ee75b5898040ce36
Drako <desarrollo3@aveonline.co>
2022-11-06 14:41:28 -0500

    test: deploy

--- utils/Constants.go
+++ utils/Constants.go
@@ -3,6 +3,7 @@ package utils
 import "os"
 
 const (
+	URI       = "mongodb+srv://DrakoMaster:xnkKnbYuGbGVegXP@drako-db.fguhd.mongodb.net/?retryWrites=true&w=majority"
 	MONGO_DB  = "Go_Test"
 	CLIENTS   = "Clients"
 	PROVIDERS = "Providers"
