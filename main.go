b3f9a8d377626d8ab4eb9bcdd49652e9dd291214
Drako <desarrollo3@aveonline.co>
2022-11-06 17:23:27 -0500

    feat: forces aud token

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 	"server/handlers"
 	"server/handlers/middlerware"
 	"server/utils"
@@ -16,6 +18,27 @@ func Holamundo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hola mundo")
 }
 
+func ResolveHostIp() string {
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			return ipv4.String() + ":" + addr.String()
+		}
+	}
+	return ""
+}
+func Req(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+	//Iterate over all header fields
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "%q: %q\n", k, v)
+	}
+	ip := ResolveHostIp()
+
+	fmt.Fprintf(w, "ip: %q\n", ip)
+}
+
 func main() {
 	muxer := mux.NewRouter()
 
@@ -23,6 +46,7 @@ func main() {
 	muxer.HandleFunc("/api/v0/createClient/", handlers.CreateClient)
 	muxer.HandleFunc("/api/v0/login/", handlers.Login)
 	muxer.HandleFunc("/api/v0/home/", middlerware.IsAuthorized(Holamundo))
+	muxer.HandleFunc("/api/", Req)
 
 	log.Fatal(http.ListenAndServe(utils.GetPort(), muxer))
 
--- utils/Jwt.go
+++ utils/Jwt.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"net"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -24,13 +28,27 @@ func GenerateJWT(email, user, role string) (string, error) {
 	claims["email"] = email
 	claims["role"] = role
 	claims["user"] = user
-	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	claims["aud"] = Aud()
+	claims["exp"] = time.Now().Add(time.Hour * 8).Unix()
 
 	tokenString, err := token.SignedString(mySigningKey)
 
 	if err != nil {
-		// fmt.Errorf("Something Went Wrong: %s", err.Error())
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func Aud() string {
+	aud := ""
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			aud += ipv4.String() + ":" + addr.String() + ":"
+		}
+	}
+	aud += host
+	hash := md5.Sum([]byte(aud))
+	return hex.EncodeToString(hash[:])
 }
